d30691e9aea974b752e5241b83884a51bb6b7afb
wanghai2023 <21_dong@163.com>
2023-09-27 15:48:26 +0800

    Message

--- hello.py
+++ hello.py
@@ -7,7 +7,7 @@ print("文件名: ", fo.name)
 #print "是否已关闭 : ", fo.closed
 #print "访问模式 : ", fo.mode
 line = fo.readline()
-print("aaa读取第一行 %s" % (line))
+print("读取第一行 %s" % (line))
 line = fo.readline(5)
 print("读取第一行 %s" % (line))
 fo.close()
