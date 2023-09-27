a5e7bea7e5f4e3ba5014405b88b147c69e25aa9e
wanghai2023 <21_dong@163.com>
2023-09-27 11:45:25 +0800

    Publish to GitHub

---
+++ hello.py
@@ -0,0 +1,13 @@
+print('Hellowork干1')
+#str = input("请输入：")
+#print("你输入的内容是: ", str)
+fo = open("foo.txt", "r+")
+#fo.write( "www.runoob.com!\nVery good site!\n")
+print("文件名: ", fo.name)
+#print "是否已关闭 : ", fo.closed
+#print "访问模式 : ", fo.mode
+line = fo.readline()
+print("读取第一行 %s" % (line))
+line = fo.readline(5)
+print("读取第一行 %s" % (line))
+fo.close()
